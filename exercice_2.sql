846bf90d3c581a50446cec51a24950d94f4a5ef9
Khun <khunly@bstorm.be>
2026-05-07 16:38:45 +0200

    exercice 2

--- exercice_2.sql
+++ exercice_2.sql
@@ -2,7 +2,7 @@ select
     supplier_id,
     avg(unit_price)
 from products
-group by supplier_id
+group by supplier_id;
 
 -- 4. Chiffre d'affaires par client
 -- Calculez le montant total
@@ -11,25 +11,25 @@ group by supplier_id
 -- Affichez le company_name
 -- du client et le total arrondi.
 
-SELECT
+select
     round(SUM(unit_price*quantity) * 100) / 100 as total,
     c.company_name
-FROM order_details od
-JOIN orders o ON o.order_id = od.order_id
-JOIN customers c ON c.customer_id = o.customer_id
-GROUP BY c.company_name
+from order_details od
+join orders o on o.order_id = od.order_id
+join customers c on c.customer_id = o.customer_id
+group by c.company_name;
 
 --6. Frais de port par transporteur
 -- Calculez les frais de port moyens (freight)
 -- pour chaque transporteur.
 -- Affichez le nom du transporteur (shippers.company_name).
 
-SELECT
-    AVG(o.freight),
+select
+    avg(o.freight),
     s.company_name
-FROM orders o
-JOIN shippers s ON s.shipper_id = o.ship_via
-GROUP BY s.company_name
+from orders o
+join shippers s on s.shipper_id = o.ship_via
+group by s.company_name;
 
 --8. Comparaison au maximum de la commande
 -- Pour chaque ligne de la table order_details,
@@ -37,7 +37,7 @@ GROUP BY s.company_name
 -- le prix unitaire du produit actuel
 -- et, dans une colonne supplémentaire,
 -- le prix du produit le plus cher de cette même commande.
-SELECT
+select
     order_id,
     -- unit_price,
     -- max(unit_price) over (partition by order_id)
@@ -46,7 +46,7 @@ SELECT
             then 0
         else unit_price
     end
-FROM order_details
+from order_details;
 
 -- 7. Classement des prix par catégorie
 -- Affichez le nom du produit,
@@ -60,7 +60,7 @@ SELECT
     product_name,
     unit_price,
     rank() over (partition by category_id order by unit_price) as rank
-FROM products
+FROM products;
 
 -- 10. Délai entre deux commandes
 -- Pour chaque client, affichez l'ID de la commande,
@@ -74,19 +74,22 @@ select
     order_id,
     order_date,
     lag(order_date) over (partition by customer_id order by order_date)
-from orders
+from orders;
 
 -- 12. Top 3 des ventes par catégorie
 -- Listez les 3 produits les plus vendus
 -- (en termes de quantité totale) pour chaque catégorie.
-select *
-FROM (SELECT p.product_name,
-             category_name,
-             sum(quantity),
-             rank() over (partition by category_name order by sum(quantity) DESC) as rank
-      FROM categories c
-               JOIN products p on p.category_id = c.category_id
-               JOIN order_details od on od.product_id = p.product_id
-      group by product_name, category_name
-      order by category_name
-) where rank < 4
+select
+    *
+from (
+    select
+        p.product_name,
+        category_name,
+        sum(quantity),
+        rank() over (partition by category_name order by sum(quantity) desc) as rank
+    from categories c
+    join products p on p.category_id = c.category_id
+    join order_details od on od.product_id = p.product_id
+    group by product_name, category_name
+    order by category_name
+) sub where rank < 4
